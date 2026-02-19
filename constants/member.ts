1566ed9baba01bbcc0ec98aa078a70b78d260063
Hakim-pixel <rahmadciamis@gmail.com>
2026-02-20 06:17:15 +0700

    push pertama

---
+++ constants/member.ts
@@ -0,0 +1,8 @@
+export const MEMBERS = [
+    {
+        name: 'Hakim',
+        quote: 'dingin ciut hangat besar',
+        instagram: '@hakimm.png',
+        images: '/images/members/Hakim.jpeg'
+    },
+]
